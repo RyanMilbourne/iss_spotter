c630830289229937d3ff412a737de2fae60b81cb
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-21 11:32:11 -0700

    find out user ip address and lat + long coordinates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -6,4 +6,12 @@ fetchMyIP((error, ip) => {
     return;
   }
   console.log(ip);
+
+  fetchCoordsByIP(ip, ((error, coordinates) => {
+    console.log(coordinates);
+  }));
+
+
 });
+
+
