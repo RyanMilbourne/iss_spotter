01af8c7cdb1d56b3263eda2ad4f6ac7dc4dec121
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-22 11:33:52 -0700

    find out when(date/time) the ISS passes over your location

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log(`Error: `, error);
-    return;
+const printPassTimes = function(passTimes) {
+  // loop through passTimes array
+  for (const pass of passTimes) {
+    // collect Date information and assign to const for simplicity
+    const datetime = new Date(0);
+    // convert date/time into universal time
+    datetime.setUTCSeconds(pass.risetime);
+    // collect pass duration from object and assign to const for simplicity
+    const duration = pass.duration;
+    // print corresponding information
+    console.log(`The next pass of the ISS is at ${datetime} for ${duration} seconds!`);
   }
-  console.log(ip);
+};
 
-  fetchCoordsByIP(ip, ((error, coordinates) => {
-    if (error) {
-      console.log(`Error: `, error);
-      return;
-    }
-    console.log(coordinates);
+nextISSTimesForMyLocation((err, passTimes) => {
+  // return error if error exists
+  if (err) {
+    return console.log('Error: It did not work', err);
+  }
 
-    fetchISSFlyOverTimes(coordinates, ((error, passes) => {
-      if (error) {
-        console.log(`Error: `, error);
-        return;
-      }
-      console.log(passes)
-    }))
+  // print a message & call printPassTimes function if no error
+  console.log('\nBased on your IP, this is when you can expect to see the ISS: \n');
 
-  }));
-
+  printPassTimes(passTimes);
 });
